04178717a3368942d56f7733b3e704506bd20cc5
Vince Lavang <vincelavang@gmail.com>
2017-07-06 10:16:20 -0700

    Require fs module. #2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+const fs = require('fs')
 
 const notes = []
 
