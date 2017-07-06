12f4125c7c10a0ae32931ec37bee4c135c60a9d0
Vince Lavang <vincelavang@gmail.com>
2017-07-05 21:09:37 -0700

    Created empty array and register GET route. #1

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+
+const notes = []
+
+app.get('/notes', function (req, res) {
+  res.json(notes)
+})
